32a5da93ae1a8ce48c6d6b8eb3af2841dc919862
liki 521 <chlikitha260@gmail.com>
2022-11-15 12:26:01 +0530

    push the calci

---
+++ Calculator/Calci.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Calculator
+{
+    public class Calci
+    {
+        public int Add(int x,int y)
+        {
+            return x + y;
+        }
+        public int Multiply(int x,int y)
+        {
+            return x * y;
+
+        }
+    }
+
+}
